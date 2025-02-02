bc7e50ea05d4f666a61d7eb32ed3a2bb1c4df635
arahm7777 <mariasohail200@gmail.com>
2025-02-02 18:52:37 +0500

    Add files via upload

---
+++ faction.py
@@ -0,0 +1,30 @@
+def intro():
+    print("arham")
+intro()
+#method2
+def add(number1,number2):
+    reult=number1+number2
+    print("the answer is: reult")
+
+def sub(number1,number2):
+    reult=number1-number2
+    print("the answer is: reult")
+
+def mul(number1,number2):
+    reult=number1*number2
+    print("the answer is: reult")
+
+def dvl(number1,number2):
+    reult=number1/number2
+    print("the answer is: reult")
+
+
+
+
+
+number1 = int(input("enter number1"))
+number2 = int(input("enter number2"))
+add(): # type: ignore
+sub(): # type: ignore
+mul(): # type: ignore
+dev(): # type: ignore
